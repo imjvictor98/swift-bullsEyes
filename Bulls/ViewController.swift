fa0404258ce3fd00d3a1fd485222f360aca832e2
imjvictor98 <jv.camara@hotmail.com>
2019-11-08 13:54:43 -0300

    Initial Commit

---
+++ Bulls/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bulls
+//
+//  Created by João Victor on 08/11/19.
+//  Copyright © 2019 João Victor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
